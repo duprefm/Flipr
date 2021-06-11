eaaaec5719c8beb16aa3386305f50ebe92b1895a
Fabrice Dupre <duprefm@gmail.com>
2021-06-11 23:35:29 +0200

    Get auth token and datas from Flipr

--- Get_Token.py
+++ Get_Token.py
@@ -2,21 +2,35 @@
 
 import requests
 import json
+import sys
 
-BASE_URL = 'https://apis.goflipr.com/modules/1A1C74/survey/last'
-token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZHVwcmVmbUBnbWFpbC5jb20iLCJzdWIiOiJkdXByZWZtQGdtYWlsLmNvbSIsIm5iZiI6MTYyMzI3MzQyMiwiZXhwIjoxNjQ2NjAxNDIyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0LyIsImF1ZCI6IkFueSJ9.toikTjcbG7Sq9qYVeMowAXpXiCz6FAoMcpmb_S4HPwQ"
+BASE_URL_GET_TOKEN = 'https://apis.goflipr.com/OAuth2/token?grant_type=password&password=Ght1v%26l0&username=duprefm@gmail.com'
+BASE_URL_GET_DATA = 'https://apis.goflipr.com/modules/1A1C74/survey/LastHours/24'
+token = ""
+username = "duprefm@gmail.com"
+password = "Ght1v&l0"
+
+payload = {
+    'grant_type': 'password',
+    'username': username,
+    'password': password
+}
+
+r = requests.post("https://apis.goflipr.com/OAuth2/token", 
+    headers={"Content-Type":"application/x-www-form-urlencoded"},
+    data=payload)
+
+json_data = r.json()
+
+x = json.dumps(json_data, indent=4, sort_keys=True)
+
+y = json.loads(x)
+token = (y["access_token"])
 
 headers = {'Authorization': "Bearer {}".format(token)}
-auth_response = requests.get(BASE_URL, headers=headers)
+auth_response = requests.get(BASE_URL_GET_DATA, headers=headers)
 
-print(auth_response.json())
+#print(auth_response.json())
 
-# some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
-print(y["age"])
-print(y)
+with open('FliprData.json', 'w') as f:
+    print(auth_response.json(), file=f)
